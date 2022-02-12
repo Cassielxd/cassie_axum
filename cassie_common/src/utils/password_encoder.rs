04c1ff2d9cc52f0449046f55bb61e6ddc42096e0
String <348040933@qq.com>
2022-02-12 19:40:37 +0800

    删除冗余代码

--- cassie_common/src/utils/password_encoder.rs
+++ cassie_common/src/utils/password_encoder.rs
@@ -20,30 +20,3 @@ impl PasswordEncoder {
     }
 }
 
-///测试模块
-#[cfg(test)]
-mod test {
-    use cassie_common::utils::password_encoder::PasswordEncoder;
-
-    ///测试密码 编码和解码
-    #[test]
-    fn test_encode() {
-        let s = PasswordEncoder::encode("123456");
-        println!("{}", s);
-        assert_eq!(
-            PasswordEncoder::encode("123456"),
-            PasswordEncoder::encode("123456")
-        )
-    }
-
-    #[test]
-    fn test_verify() {
-        let password = "12345";
-        let raw_password = "12345";
-
-        assert!(PasswordEncoder::verify(password, raw_password));
-
-        let encode_password = PasswordEncoder::encode(password);
-        assert!(PasswordEncoder::verify(&encode_password, password));
-    }
-}
