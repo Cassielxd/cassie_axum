7a398fbe967ce745c22d3c9fab45105559a2d9cc
“18591992168” <348040933@qq.com>
2022-03-01 17:25:38 +0800

    添加动态表单值验证

--- cassie_web/src/admin/asi/asi_group_values_controller.rs
+++ cassie_web/src/admin/asi/asi_group_values_controller.rs
@@ -4,13 +4,14 @@ use axum::{
     Json,
 };
 use cassie_common::{error::Error, RespVO};
+use validator::Validate;
 
 use crate::{
-    dto::asi_dto::{AsiGroupColumnDTO, AsiGroupValuesDTO},
+    
     entity::PageData,
     request::AsiQuery,
     service::crud_service::CrudService,
-    CONTEXT,
+    CONTEXT, dto::asi_dto::AsiGroupValuesDTO,
 };
 
 pub async fn page(arg: Option<Query<AsiQuery>>) -> impl IntoResponse {
@@ -35,6 +36,12 @@ pub async fn get_by_id(Path(id): Path<String>) -> impl IntoResponse {
 }
 
 pub async fn save(Json(arg): Json<Vec<AsiGroupValuesDTO>>) -> impl IntoResponse {
+    /*执行验证逻辑*/
+    for dto in &arg {
+        if let Err(e) = dto.validate() {
+            return RespVO::<()>::from_error("-1", &Error::E(e.to_string())).resp_json();
+        }
+    }
     CONTEXT.asi_service.asi_values.save_batch_values(arg).await;
     RespVO::from_result(&Ok("保存成功".to_string())).resp_json()
 }
