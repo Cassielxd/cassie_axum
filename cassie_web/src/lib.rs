ce3e3305b33b164943e8ee0792f182a3a5771f26
String <348040933@qq.com>
2022-03-30 22:52:34 +0800

    添加注释

--- cassie_web/src/lib.rs
+++ cassie_web/src/lib.rs
@@ -1,15 +1,22 @@
 #![allow(unused_variables)] //允许未使用的变量
 #![allow(dead_code)] //允许未使用的代码
 #![allow(unused_must_use)]
-
+//配置
 pub mod config;
+//管理端
 pub mod admin;
+//权限模块
 pub mod cici_casbin;
+//中间件
 pub mod middleware;
 //nacos注册中心默认实现
 pub mod nacos;
+//路由
 pub mod routers;
+//总服务入口
 pub mod service;
+//前端接口
+pub mod api;
 
 use cassie_orm::dao::{init_mongdb, init_rbatis};
 use mongodb::Database;
@@ -18,7 +25,15 @@ use crate::cici_casbin::casbin_service::CasbinService;
 use crate::service::ServiceContext;
 use cassie_config::config::ApplicationConfig;
 use state::Container;
-
+/*
+整个项目上下文ApplicationContext
+包括：
+ApplicationConfig 配置
+Database mongodb数据库
+Rbatis  mysql orm
+ServiceContext 服务上下文
+CasbinService 权限服务
+*/
 pub static APPLICATION_CONTEXT: Container![Send + Sync] = <Container![Send + Sync]>::new();
 
 /*初始化环境上下文*/
