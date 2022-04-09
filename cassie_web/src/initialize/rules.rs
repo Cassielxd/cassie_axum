49048e6a935f98e369dad8983d4389a75ad5c7ba
String <348040933@qq.com>
2022-04-10 00:09:13 +0800

    ..

--- cassie_web/src/initialize/rules.rs
+++ cassie_web/src/initialize/rules.rs
@@ -1,16 +1,4 @@
-use cassie_config::config::ApplicationConfig;
-use cassie_rules::core::rules::RulesContext;
-
-use crate::APPLICATION_CONTEXT;
-
-pub fn get_config() -> &'static ApplicationConfig {
-    APPLICATION_CONTEXT.get::<ApplicationConfig>()
-}
-
 //初始话规则引擎
 pub fn init_rules() {
-    //创建默认规则对象
-    let mut rule_context = RulesContext::default();
-    rule_context.engine.register_fn("get_config", get_config);
-    //加载所有规则
+    todo!("待开发")
 }
