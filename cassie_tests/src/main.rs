c7c801711ec4e0ba57ed17cdb8061d07445b1922
“18591992168” <348040933@qq.com>
2022-05-13 17:24:20 +0800

    。。。

--- cassie_tests/src/main.rs
+++ cassie_tests/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::Read;
+use gag::BufferRedirect;
 fn main() {
-    println!("Hello, world!");
+    let mut buf = BufferRedirect::stdout().unwrap();
+    println!("Hello world!");
+    println!("Hello world1!");
+    println!("Hello world2!");
+
+    let mut output = String::new();
+    buf.read_to_string(&mut output).unwrap();
+    drop(buf);
+    println!("console:{}", &output[..]);
 }
--- cassie_web/src/service/wechat/wxapp_service.rs
+++ cassie_web/src/service/wechat/wxapp_service.rs
@@ -28,10 +28,12 @@ pub async fn save_or_update_user(user: WechatUserDTO) -> i64 {
         let list = wechat_user_service.fetch_list_by_column(WechatUser::unionid(), &vec![unionid.clone()]).await.unwrap();
 
         if list.len() > 0 {
-            let uid = list.get(0).unwrap().id().clone().unwrap();
+            let mut userinfo = list.get(0).unwrap().clone();
+            let uid = userinfo.id().clone().unwrap();
+            userinfo.set_nickname(user.nickname().clone());
+            userinfo.set_headimgurl(user.headimgurl().clone());
             //执行更新逻辑
-            wechat_user_service.update_by_id(uid.to_string(), &mut user.clone().into()).await;
-            user_service.update_by_id(uid.to_string(), &mut build_user_info(&user).into()).await;
+            wechat_user_service.update_by_id(uid.to_string(), &mut userinfo.into()).await;
             uid
         } else {
             0
@@ -44,10 +46,10 @@ pub async fn save_or_update_user(user: WechatUserDTO) -> i64 {
         uid = if let Some(routine_openid) = user.routine_openid() {
             let list = wechat_user_service.fetch_list_by_column(WechatUser::routine_openid(), &vec![routine_openid.clone()]).await.unwrap();
             if list.len() > 0 {
-                let uid = list.get(0).unwrap().id().clone().unwrap();
+               let  userinfo = list.get(0).unwrap();
+                let uid = userinfo.id().clone().unwrap();
                 //执行更新逻辑
                 wechat_user_service.update_by_id(uid.to_string(), &mut user.clone().into()).await;
-                user_service.update_by_id(uid.to_string(), &mut build_user_info(&user).into()).await;
                 return uid;
             }
             0
