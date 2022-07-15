f8634fcafb28d7f0591266cb2e9cff388c8713cb
Cassie <348040933@qq.com>
2022-07-15 11:06:16 +0800

    ..

--- cassie_msg/src/sms/dayu.rs
+++ cassie_msg/src/sms/dayu.rs
@@ -1,8 +1,3 @@
 use crate::sms::sms_service::ISmsService;
 
 pub struct AliDaYuSmsService {}
-impl ISmsService for AliDaYuSmsService {
-    fn sms_send(&self, mobiles: &vec![str], v: &str) -> cassie_common::error::Result<String> {
-        todo!()
-    }
-}
