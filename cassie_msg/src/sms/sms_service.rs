c30c6caedaa1e59fffbcc494f6e1b1ee0bd32edb
Cassie <348040933@qq.com>
2022-09-15 18:45:45 +0800

    编译不通过BUG

--- cassie_msg/src/sms/sms_service.rs
+++ cassie_msg/src/sms/sms_service.rs
@@ -1,7 +1,5 @@
 use cassie_common::error::{Error, Result};
-pub trait ISmsService {
-    fn sms_send(&self, mobiles: &vec![str], v: &str) -> Result<String>;
-}
+pub trait ISmsService {}
 
 pub struct SmsService {
     pub inner: Box<dyn ISmsService>,
